0af96c7b4902033e98551243fbd988e876f707b7
Juliano <cont.juliano@outlook.com>
2025-04-01 19:10:45 -0300

    feat: adiciona ajuste nonce jwt

--- keycloak/auth/main.go
+++ keycloak/auth/main.go
@@ -1,29 +1,74 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"github.com/go-resty/resty/v2"
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
 )
+
+type ResponseKeycloakCallback struct {
+	AccessToken      string `json:"access_token"`
+	ExpiresIn        int    `json:"expires_in"`
+	RefreshExpiresIn int    `json:"refresh_expires_in"`
+	RefreshToken     string `json:"refresh_token"`
+	TokenType        string `json:"token_type"`
+	IDToken          string `json:"id_token"`
+	SessionState     string `json:"session_state"`
+	Scope            string `json:"scope"`
+}
+
+type DecodedIdToken struct {
+	Nonce string `json:"nonce"`
+}
 
 func main() {
 	r := gin.Default()
 	client := resty.New()
 
+	store := cookie.NewStore([]byte("secret"))
+	store.Options(sessions.Options{
+		Path:     "/",
+		MaxAge:   3600 * 8, // 8 hours
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode,
+	})
+	r.Use(sessions.Sessions("mysession", store))
+
 	r.GET("/login", func(ctx *gin.Context) {
+		session := sessions.Default(ctx)
+		nonce := uuid.NewString()
+		if session.Get("nonce") == nil {
+			session.Set("nonce", nonce)
+			if err := session.Save(); err != nil {
+				ctx.AbortWithError(http.StatusInternalServerError, err)
+				return
+			}
+		} else {
+			nonce = session.Get("nonce").(string)
+		}
 		query := url.Values{}
 		query.Set("client_id", "fullcycle-client")
 		query.Set("redirect_uri", "http://localhost:8081/callback")
 		query.Set("scope", "openid")
 		query.Set("response_type", "code")
+		query.Set("nonce", nonce)
 		location := fmt.Sprintf("http://localhost:8080/realms/fullcycle/protocol/openid-connect/auth?%s", query.Encode())
 		ctx.Redirect(http.StatusTemporaryRedirect, location)
 	})
 
 	r.GET("/callback", func(ctx *gin.Context) {
+		session := sessions.Default(ctx)
+		nonce := session.Get("nonce").(string)
+
 		body := map[string]string{
 			"client_id":    "fullcycle-client",
 			"grant_type":   "authorization_code",
@@ -32,16 +77,59 @@ func main() {
 			"scope":        "openid",
 		}
 		url := "http://localhost:8080/realms/fullcycle/protocol/openid-connect/token"
+		var result *ResponseKeycloakCallback
 		response, err := client.R().
 			SetFormData(body).
+			SetResult(&result).
 			Post(url)
 		if err != nil {
-			ctx.AbortWithStatusJSON(response.StatusCode(), map[string]any{
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, map[string]any{
 				"err": "invalid login",
 			})
 			return
 		}
-		ctx.JSON(http.StatusOK, string(response.Body()))
+
+		if response.StatusCode() != http.StatusOK {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, map[string]any{
+				"err": "invalid login",
+			})
+			return
+		}
+
+		idToken := result.IDToken
+		parts := strings.Split(idToken, ".")
+		if len(parts) != 3 {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, map[string]any{
+				"err": "invalid id token format",
+			})
+			return
+		}
+
+		payload, err := jwt.DecodeSegment(parts[1])
+		if err != nil {
+			ctx.AbortWithStatusJSON(response.StatusCode(), map[string]any{
+				"err": "error to decode token",
+			})
+			return
+		}
+
+		var decodedIdToken *DecodedIdToken
+
+		if err := json.Unmarshal(payload, &decodedIdToken); err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, map[string]any{
+				"err": "error to unmarshal token",
+			})
+			return
+		}
+
+		if decodedIdToken.Nonce != nonce {
+			ctx.AbortWithStatusJSON(http.StatusForbidden, map[string]any{
+				"err": "user not allowed",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, result)
 	})
 
 	http.ListenAndServe(":8081", r)
